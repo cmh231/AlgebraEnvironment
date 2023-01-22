0a49977aacc4f96f663da3a2e2654af8214524a4
Calvin Hooper <cmh231@cam.ac.uk>
2023-01-22 17:17:40 +0000

    Corrected typing in AnonymousProperty.py in line with previous commit. (I'd just forgotten that class existed!)

--- AnonymousProperty.py
+++ AnonymousProperty.py
@@ -1,8 +1,9 @@
 from PropertyInheritance import PropertyInheritance
+from typing import List
 
 class AnonymousProperty(PropertyInheritance):
     """Anonymous property used if one object inherits multiple unrelated properties."""
 
-    def __init__(self, inheritedProperties: list['PropertyInheritance']):
+    def __init__(self, inheritedProperties: List['PropertyInheritance']):
         # A fixed name can be used since anonymous properties are, by definition defined only by those they contain.
         super().__init__("λ", inheritedProperties)
